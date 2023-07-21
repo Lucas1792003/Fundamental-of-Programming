f23af70a41f5a4c74ef9730b698ed005e5e218ed
Lucas <122111166+Lucas1792003@users.noreply.github.com>
2023-07-21 17:09:52 +0700

    Add files via upload

---
+++ Midterm/Q1.py
@@ -0,0 +1,6 @@
+#6511171_Wai_Yan_Paing_542_Practical_Midterm_as1
+amount = float(input('Enter the volume of the crude oil in liter(s): '))
+#97.59usd = 158.98 liter
+#1liter = 22.443
+ans = amount * 22.44238520568625
+print(F'The {amount} Liter(s)crude oil will cost {ans} in Thai Bath(s).')
