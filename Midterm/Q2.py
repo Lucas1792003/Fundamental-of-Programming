e7394677447eb78c322f09519a2a0dbc7c940d93
Lucas <122111166+Lucas1792003@users.noreply.github.com>
2024-08-04 23:23:41 +0700

    Q2.py

--- Midterm/Q2.py
+++ Midterm/Q2.py
@@ -1,11 +1,8 @@
-
 word = input('Enter a word: ')
 i = word.upper()
+
 print(i)
-print(f'***********\n',end="")
-print(f'**={i}=**\n',end="")
-print(f'**={i}=**\n',end="")
-print(f'**={i}=**\n',end="")
-print(f'**={i}=**\n',end="")
-print(f'**={i}=**\n',end="")
-print(f'***********',end ="")
+print('***********')
+for _ in range(5):
+    print(f'**={i}=**')
+print('***********')
